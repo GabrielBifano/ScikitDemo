b13359c72792de196e8b939714d58b6d73728ce5
GabrielBifano <gabrielfev@outlook.com>
2023-10-27 14:55:42 -0300

    Generator class that can create and print the graphic representation of the dataset

---
+++ src/generator.py
@@ -0,0 +1,66 @@
+from sklearn.datasets import make_blobs
+from sklearn.datasets import make_moons
+from sklearn.datasets import make_circles
+from sklearn.datasets import make_s_curve
+from graphic import Graphic, Plot
+import random
+
+
+class Generator:
+    def __init__(self, samp=300, feat=2, clusters=6, noise=0.05, factor=0.5):
+        self.g = Graphic()
+        self.samp = samp
+        self.feat = feat
+        self.clusters = clusters
+        self.noise = noise
+        self.factor = factor
+        self.rand = random.randint(0, 0xFFFFFFFF - 1)
+
+
+    def make_blobs(self, to_print: bool):
+        X, y = make_blobs (
+            n_samples=self.samp,
+            n_features=self.feat,
+            centers=self.clusters,
+            random_state=self.rand
+        )
+        if to_print:
+            self.g.select(Plot.cluster).points_2D(X=X, y=y)
+            self.g.select(Plot.indistinguishable).points_2D(X=X)
+        return X, y
+
+
+    def make_moons(self, to_print: bool):
+        X, y = make_moons(
+            n_samples=self.samp,
+            noise=self.noise,
+            random_state=self.rand
+        )
+        if to_print:
+            self.g.select(Plot.cluster).points_2D(X=X, y=y)
+            self.g.select(Plot.indistinguishable).points_2D(X=X)
+        return X, y
+
+
+    def make_circles(self, to_print: bool):
+        X, y = make_circles(
+            n_samples=self.samp,
+            factor=self.factor,
+            noise=self.noise,
+            random_state=self.rand
+        )
+        if to_print:
+            self.g.select(Plot.cluster).points_2D(X=X, y=y)
+            self.g.select(Plot.indistinguishable).points_2D(X=X)
+        return X, y
+
+
+    def make_s_curve(self, to_print: bool):
+        X, y = make_s_curve(
+            n_samples=self.samp,
+            noise=self.noise,
+            random_state=self.rand
+        )
+        if to_print:
+            self.g.select(Plot.indistinguishable).points_2D(X=X[:,0], y=y)
+        return X, y
