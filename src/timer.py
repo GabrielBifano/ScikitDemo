9fd6c1b0907619b7a0c79f58a8d10eced5e14963
GabrielBifano <gabrielfev@outlook.com>
2023-11-14 17:20:12 -0300

    some colors to CLI output

--- src/timer.py
+++ src/timer.py
@@ -2,10 +2,18 @@ from time import perf_counter
 
 def timer(func):
 
+    B = "\033[1m"
+    R = "\033[0m"
+    CYAN = "\033[36m"
+    GREEN = "\033[92m"
+
     def wrapper(*args, **kwargs):
         init = perf_counter()
         result = func(*args, **kwargs)
         end  = perf_counter()
-        print(f'The method {func.__name__} took {(end - init):.4e} seconds to run')
+        print (
+            f'The method {B}{CYAN} {func.__name__}{R}  '
+            f'took {B}{GREEN}{(end - init):.4e}{R} seconds to run'
+        )
         return result
     return wrapper
