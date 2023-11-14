7487099b70024f793c292a8fdf1e800712f389d4
GabrielBifano <gabrielfev@outlook.com>
2023-11-14 17:18:58 -0300

    now sample accepts DataFrame as entry

--- src/graphic.py
+++ src/graphic.py
@@ -1,5 +1,5 @@
-from enum import Enum
 import matplotlib.pyplot as plt
+import pandas as pd
 
 class Graphic:
   def __init__(self):
@@ -9,6 +9,7 @@ class Graphic:
     self.__default_color    = "black"
 
   def sample(self, X, y=[None]) -> None:
+    if type(X) == pd.DataFrame: X = X.values
     if y.all() == None: y = [0]*len(X)
     plt.scatter(X[:, 0], X[:, 1], c=y, cmap='viridis', marker='.')
     plt.title("Scatter plot of samples in dataset")
