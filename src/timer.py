e195e1f8a5f443cbeaa4471888ef233f76c354cd
GabrielBifano <gabrielfev@outlook.com>
2023-11-15 18:21:07 -0300

    exported ansi escaped to other file

--- src/timer.py
+++ src/timer.py
@@ -1,19 +1,15 @@
 from time import perf_counter
+from ansi import BOLD, RESET, CYAN, GREEN
 
-def timer(func):
-
-    B = "\033[1m"
-    R = "\033[0m"
-    CYAN = "\033[36m"
-    GREEN = "\033[92m"
+def timer(func):    
 
     def wrapper(*args, **kwargs):
         init = perf_counter()
         result = func(*args, **kwargs)
         end  = perf_counter()
         print (
-            f'The method {B}{CYAN} {func.__name__}{R}  '
-            f'took {B}{GREEN}{(end - init):.4e}{R} seconds to run'
+            f'The method {BOLD}{CYAN} {func.__name__}{RESET}  '
+            f'took {BOLD}{GREEN}{(end - init):.4e}{RESET} seconds to run'
         )
         return result
     return wrapper
