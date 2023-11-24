b810ae16bc7c2f31175042ea1e478f93eef94d16
GabrielBifano <gabrielfev@outlook.com>
2023-11-24 00:36:25 -0300

    external metrics added

--- src/scorer.py
+++ src/scorer.py
@@ -2,13 +2,16 @@ import pandas as pd
 from graphic import Graphic
 from ansi import ANSI
 
-from sklearn.cluster import KMeans
+from sklearn.metrics import rand_score
+from sklearn.metrics import v_measure_score
 from sklearn.metrics import silhouette_score
-from sklearn.metrics import calinski_harabasz_score
-from sklearn.metrics import davies_bouldin_score
+from sklearn.metrics import mutual_info_score
 from sklearn.metrics import homogeneity_score
 from sklearn.metrics import completeness_score
-from sklearn.metrics import v_measure_score
+from sklearn.metrics import adjusted_rand_score
+from sklearn.metrics import davies_bouldin_score
+from sklearn.metrics import fowlkes_mallows_score
+from sklearn.metrics import calinski_harabasz_score
 from sklearn.metrics.cluster import contingency_matrix
 
 class Scorer:
@@ -27,9 +30,31 @@ class Scorer:
     def contingency_matrix(self, *args):
         pass
 
-    #TODO implement this method
-    def external_metrics(self, truth_data, predict_data) -> dict:
-        pass
+    def external_metrics(self, truth, predicted) -> dict:
+        metrics_dict = {
+            'rand_score': rand_score(truth, predicted),
+            'v_measure_score': v_measure_score(truth, predicted),
+            'mutual_info_score': mutual_info_score(truth, predicted),
+            'homogeneity_score': homogeneity_score(truth, predicted),
+            'completeness_score': completeness_score(truth, predicted),
+            'adjusted_rand_score': adjusted_rand_score(truth, predicted),
+            'fowlkes_mallows_score': fowlkes_mallows_score(truth, predicted),
+        }
+        if self.print_cli:
+            a = ANSI()
+            print(
+                f'Model performance {a.b}{a.green}external metrics:{a.res}'
+                f'{a.cyan}'
+                f'\n\t* rand_score:             {metrics_dict["rand_score"]:.5}'
+                f'\n\t* adjusted_rand_score:    {metrics_dict["adjusted_rand_score"]:.5}'
+                f'\n\t* v_measure_score:        {metrics_dict["v_measure_score"]:.5}'
+                f'\n\t* mutual_info_score:      {metrics_dict["mutual_info_score"]:.5}'
+                f'\n\t* homogeneity_score:      {metrics_dict["homogeneity_score"]:.5}'
+                f'\n\t* completeness_score:     {metrics_dict["completeness_score"]:.5}'
+                f'\n\t* fowlkes_mallows_score:  {metrics_dict["fowlkes_mallows_score"]:.5}'
+                f'{a.res}'
+            )
+        return metrics_dict
 
     def internal_metrics(self, data: pd.DataFrame, labels) -> dict:
         metrics_dict = {
@@ -40,7 +65,7 @@ class Scorer:
         if self.print_cli:
             a = ANSI()
             print(
-                f'Model performance {a.b}{a.green}metrics:{a.res}'
+                f'Model performance {a.b}{a.green}internal metrics:{a.res}'
                 f'{a.cyan}'
                 f'\n\t* silhouette_score:       {metrics_dict["silhouette_score"]:.5}'
                 f'\n\t* davies_bouldin_score:   {metrics_dict["davies_bouldin_score"]:.5}'
